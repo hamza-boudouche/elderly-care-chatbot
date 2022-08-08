19e148c86d4e0fbc00fcb6dbb062f79d4c053f8d
hamza-boudouche <boudouche.hamza.13.07.2000@gmail.com>
2022-08-08 16:56:37 +0100

    format messages from wolfram alpha api

--- chatbot/actions/utils/ActionAdditionalInfo.py
+++ chatbot/actions/utils/ActionAdditionalInfo.py
@@ -7,6 +7,24 @@ CALENDAR_URL = "http://localhost:3000"
 WOLFRAM_ALPHA_APP_ID = "J937X3-JAV9UYR7QJ"
 
 
+def parse_markdown(table):
+    if "|" not in table:
+        return table
+    if table[0] == "|":
+        table = "\U00002800" + table
+    rows = table.split("\n")
+    max_separator = 0
+    for row in rows:
+        nb_separator = row.count("|")
+        if nb_separator > max_separator:
+            max_separator = nb_separator
+    row_separator = ["|" for i in range(max_separator)]
+    row_separator = ":---:" + ":---:".join(row_separator) + ":---:"
+    rows.insert(1, row_separator)
+    rows = "\n".join(rows)
+    return rows
+
+
 async def actionAdditionalInfo(search_query):
     messages = []
     if(search_query is None):
@@ -27,14 +45,17 @@ async def actionAdditionalInfo(search_query):
                         title = subpod.get("@title")
                         if title == "":
                             title = pod.get("@title")
-                        if title != 'Input interpretation':
+                        if title != 'Input interpretation' and title != 'Wikipedia summary':
                             useful_info[title] = text
-            messages = useful_info
+            for key, value in useful_info.items():
+                value = parse_markdown(value)
+                messages.append(
+                    {
+                        "type": "markdown",
+                        "text": f"{key}:\n{value}"
+                    }
+                )
     return messages
-
-
-def parse_markdown(table):
-    pass
 
 
 # testing
@@ -47,4 +68,19 @@ if __name__ == "__main__":
         table = "full name | Elon Musk\ndate of birth | Monday, June 28, 1971 (age: 51 years)\nplace of birth | South Africa"
         res = parse_markdown(table)
         print(res)
-    asyncio.run(wrapper_test2())
+
+    async def wrapper_test3():
+        table = "full name | Elon Musk"
+        res = parse_markdown(table)
+        print(res)
+
+    async def wrapper_test4():
+        table = "|full name | Elon Musk\ndate of birth | Monday, June 28, 1971 (age: 51 years)\nplace of birth | South Africa"
+        res = parse_markdown(table)
+        print(res)
+
+    async def wrapper_test5():
+        table = "some wikipedia paragraph"
+        res = parse_markdown(table)
+        print(res)
+    asyncio.run(wrapper_test1())
