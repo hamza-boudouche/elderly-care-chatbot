5000abe41a4298988d9ab4fc802864b883d60548
hamza-boudouche <boudouche.hamza.13.07.2000@gmail.com>
2022-08-09 11:23:00 +0100

    add markdown support to frontend

--- frontend/mascir-chatbot/src/context/MessagesContext.js
+++ frontend/mascir-chatbot/src/context/MessagesContext.js
@@ -3,11 +3,15 @@ import React from 'react';
 const MessagesContext = React.createContext([
   {
     local: true,
-    text: 'Hello',
+    text: 'Helloh',
   },
   {
     local: false,
     text: 'Hey, how can I help you ?',
+  },
+  {
+    local: false,
+    text: "full name | Elon Musk\n:---:|:---:\ndate of birth | Monday, June 28, 1971 (age: 51 years)\nplace of birth | South Africa",
   },
 ]);
 
